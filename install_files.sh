7cd6648040b08b03f5c2b84d6521b8e33d7b5c37
juicedatom <juicedatom@gmail.com>
2017-12-22 01:08:23 -0500

    Updated the neovim config to use install directories.

--- install_files.sh
+++ install_files.sh
@@ -2,14 +2,20 @@
 
 # This is very primitive and error prone.
 #
-# TODO(jmanela) - Make this smarter and check to see if files exist.
+# TODO(juicedatom) - Make this smarter and check to see if files exist.
 #               - Add error handling logic and modules for different
 #                 types of files.
 
+# TODO(juicedatom) install neovim.
+
 # neovim
-mkdir -p ~/.config/nvim
-ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
+# TODO(juicedatom) -- Add safety checks.
+ln -s $(pwd)/nvim ~/.config/nvim
 
 # vimplug for neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# TODO(juicedatom) -- install neovim.
+
+# TODO(juicedatom) -- setup install for i3 config.
