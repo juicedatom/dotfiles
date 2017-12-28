651d6b4b746d16d09189b7e38578d3a59b9a1dcc
juicedatom <juicedatom@gmail.com>
2017-12-27 19:18:55 -0500

    Removed the notify-send message in mute.

--- configs/i3/scripts/ToggleVolume.sh
+++ configs/i3/scripts/ToggleVolume.sh
@@ -2,6 +2,6 @@
 
 # Toggle volume
 pactl set-sink-mute $(pacmd list-sinks |awk '/* index:/{print $3}') toggle
-notify-send 'Toggle Mute' -t 100
-
+# notify-send 'Toggle Mute' -t 100
 pkill -RTMIN+1 i3blocks
+
